2074f5c61c175a7141dd3ff68376eb4f5e82a244
Lucas Schiochet <lukasdgs23dg@gmail.com>
2025-12-23 08:35:42 -0300

    Voltar

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
-import withPWA from "next-pwa";
+import type { NextConfig } from "next";
 
-const nextConfig = {
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
